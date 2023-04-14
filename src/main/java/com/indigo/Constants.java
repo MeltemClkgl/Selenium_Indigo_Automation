e25f0f4f858dd6dd18368ce35eb5e8189fdb0442
Meltem Calikoglu <115930441+MeltemClkgl@users.noreply.github.com>
2023-04-14 18:32:07 -0400

    Update Constants.java

--- src/main/java/com/indigo/Constants.java
+++ src/main/java/com/indigo/Constants.java
@@ -6,8 +6,8 @@ public class Constants {
     //IHTIYACIM OLDUKCA BURADAN ALIP KULLANIRIM
     //BOYLELIKLE PROJEMIN BAKIMINI COK KOLAY YAPABILIRIM
 
-    public static final String EMAIL ="info@erdemcalikoglu.com";
-    public static final String PASS = "Deneme123!";
+    public static final String EMAIL ="info@xyz";
+    public static final String PASS = "abc";
     public static final String URL = "https://www.chapters.indigo.ca/en-ca/";
     public static final String KITAP ="The Analyst by John Katzenbach";
     public static final int DEFAULT_WAIT_DURATION=10;
