dbfb27866c9591cfbca5c963d99bf045364ee7ed
Meltem Calikoglu <115930441+MeltemClkgl@users.noreply.github.com>
2023-04-14 18:36:20 -0400

    Update TestSteplerim.java

--- src/main/java/com/indigo/TestSteplerim.java
+++ src/main/java/com/indigo/TestSteplerim.java
@@ -9,7 +9,7 @@ import static com.indigo.WebElements.Indigo_WebElementlerim.*;
 
 public class TestSteplerim extends ClassObjectlerim {
 
-    //TEST STEP 01- TS01
+    
     public void launchChromeBrowser_and_navigateToTheUrl() {
         try {
             System.out.println("STEP1-launchChromeBrowser_and_navigateToTheUrl step Initialized");
@@ -29,14 +29,10 @@ public class TestSteplerim extends ClassObjectlerim {
             System.out.println("STEP2-searchForTheBook_inIndigo Initialized");
 
            try {
-               //burada ilk olarak webelementi webPage buldu
-               //2.-> locate edilen webElementi "stayInTheKnowPopUpClose_button" isimli degiskene atadi
-               //3.->webDriverWait(webelement,string) methodu calisti
-               waitUtil.webDriverWait(stayInTheKnowPopUpClose_button = getWebElement("stayInTheKnowPopUpClose_button"), "visibility");//Implicitly wait
+             
+               waitUtil.webDriverWait(stayInTheKnowPopUpClose_button = getWebElement("stayInTheKnowPopUpClose_button"), "visibility");
 
-               //bir kere locate edilen ve "stayInTheKnowPopUpClose_button"e atanan webelementi artik boyle kullanabiliriz.
-               //her defasinda->getWebElement("stayInTheKnowPopUpClose_button") diye de kullanabiliriz.
-               //Ama bu sefer projemizin performansi kotu olur
+             
                stayInTheKnowPopUpClose_button.click();
 
            } catch (NoSuchElementException e){
@@ -46,19 +42,6 @@ public class TestSteplerim extends ClassObjectlerim {
             (searchBar_textBox = getWebElement("searchBar_textBox")).sendKeys(Constants.KITAP);
 
 
-            //1.yol Bu kullanim duzgun calismiyor
-            //searchBar_textBox.sendKeys(Keys.ENTER);
-
-            //actions "ClassObjectlerim" den inherit edilmistir
-            //2.yol  Bu kullanim da duzgun calismiyor
-            //actions.sendKeys(Keys.ENTER);
-
-            //3.yol- Bu kullanim duzgun calismiyor
-            //JavascriptExecutor js = (JavascriptExecutor)getDriver();
-            //String script = "document.getElementById('header__quick-search').value += '" + Keys.ENTER + "';";
-            //js.executeScript(script);
-
-            //4.yol
             Robot robot = new Robot();
             robot.keyPress(KeyEvent.VK_ENTER);
             robot.keyRelease(KeyEvent.VK_ENTER);
@@ -125,9 +108,8 @@ public class TestSteplerim extends ClassObjectlerim {
         try {
             System.out.println("STEP5 -proceedToCheckOut initialized!");
 
-            waitUtil.webDriverWait("","");//Implicitly wait
+            waitUtil.webDriverWait("","");
 
-            //HEM ATAMA/ASSIGN HEM DE ACTION YAPTIRIYORUZ
             (proceedToCheckOutButton=getWebElement("proceedToCheckOutButton")).click();
 
             System.out.println("STEP5 -proceedToCheckOut has done!");
